af44bdce767a21dbe14cf7af215c5d4dd56a2114
Craig Bucher <craigbucher@hotmail.com>
2025-10-04 19:23:48 -0400

    server options

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
+	// "os"
+	// "os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
     amqp "github.com/rabbitmq/amqp091-go"	// Go AMQP library:
@@ -31,33 +32,79 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not create channel: %v", err)
 	}
+	defer publishCh.Close()
+	// Run the PrintServerHelp function in internal/gamelogic as the server starts up so that 
+	// you can see the commands the user of the REPL can use:
+	gamelogic.PrintServerHelp()
 
-	// use the PublishJSON function to publish a message to the exchange:
-	// PublishJSON from internal/pubsub/publish.go
-	err = pubsub.PublishJSON(
-		// Use the channel you created:
-		publishCh,
-		// Use the internal/routing package's ExchangePerilDirect string for the exchange:
-		routing.ExchangePerilDirect,
-		// Use the internal/routing package's PauseKey string for the routing key:
-		routing.PauseKey,
-		// The data to send is the JSON-marshaled internal/routing's PlayingState struct, 
-		// with the IsPaused field set to true:
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
-	if err != nil {
-		log.Printf("could not publish time: %v", err)
+	// start an infinite loop:
+	for {
+		// At the beginning of the loop, use the GetInput function in internal/gamelogic to wait 
+		// for a slice of input "words" from the user. If the slice is empty, continue to the 
+		// next iteration of the loop:
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+
+		// Check the first word:
+		switch input[0] {
+			// If it's "pause", log to the console that you're sending a pause message, and publish 
+			// the pause message as you were doing before
+			case "pause":
+				fmt.Println("sending a pause message")
+				// use the PublishJSON function to publish a message to the exchange:
+				// PublishJSON from internal/pubsub/publish.go
+				err = pubsub.PublishJSON(
+					// Use the channel you created:
+					publishCh,
+					// Use the internal/routing package's ExchangePerilDirect string for the exchange:
+					routing.ExchangePerilDirect,
+					// Use the internal/routing package's PauseKey string for the routing key:
+					routing.PauseKey,
+					// The data to send is the JSON-marshaled internal/routing's PlayingState struct, 
+					// with the IsPaused field set to true:
+					routing.PlayingState{
+					IsPaused: true,
+					},
+				)
+				if err != nil {
+					log.Printf("could not publish message: %v", err)
+				}
+				fmt.Println("Pause message sent!")
+			// If it's "resume", log to the console that you're sending a resume message, and publish 
+			// the resume message as you were doing before. The only difference is that the IsPaused 
+			// field should be set to false:
+			case "resume":
+				fmt.Println("sending a resume message")
+				err = pubsub.PublishJSON(
+					publishCh,
+					routing.ExchangePerilDirect,
+					routing.PauseKey,
+					routing.PlayingState{
+						IsPaused: false,
+					},
+				)
+				if err != nil {
+					log.Printf("could not publish message: %v", err)
+				}
+				fmt.Println("Resume message sent!")
+			// If it's "quit", log to the console that you're exiting, and break out of the loop:
+			case "quit":
+				fmt.Println("Quitting. . .")
+				return
+			// If it's anything else, log to the console that you don't understand the command:
+			default:
+				fmt.Println("I don't understand that command")
+		}
 	}
-	fmt.Println("Pause message sent!")
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	// If a signal is received, print a message to the console that the program is shutting down 
-	// and close the connection:
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("RabbitMQ connection closed.")
+	// // wait for ctrl+c
+	// signalChan := make(chan os.Signal, 1)
+	// // If a signal is received, print a message to the console that the program is shutting down 
+	// // and close the connection:
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+	// fmt.Println("RabbitMQ connection closed.")
 	
 }
